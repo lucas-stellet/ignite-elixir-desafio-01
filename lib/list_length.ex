9427a4d3214c7c6f2da109ee2a08752c98dcc04d
Lucas Stellet <lustepe@gmail.com>
2021-08-24 23:47:51 -0300

    desafio finalizado

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,7 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
-  end
+  def call(list), do: length(list, 0)
+
+  defp length([], list_length), do: list_length
+
+  defp length([_hd | tl], list_length), do: length(tl, list_length + 1)
 end
